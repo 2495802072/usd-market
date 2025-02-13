11a81e86f89df704eb1f3a97f50d2d5f57a66a8f
顾霖轩 <2495802072@qq.com>
2025-02-13 16:58:38 +0800

    删除购物车，添加收藏上坪

--- src/components/Navs.tsx
+++ src/components/Navs.tsx
@@ -86,7 +86,7 @@ const Navs: React.FC = () => {
                     }}
                 </NavLink>
 
-                {/*购物车*/}
+                {/*收藏商品*/}
                 <NavLink to="/shoppingTrolley"
                          className={({isActive}) => (isActive ? 'active-link nav-link' : 'nav-link')}>
                     {({isActive}) => {
@@ -95,17 +95,16 @@ const Navs: React.FC = () => {
 
                                 <NavItemBg color={'var(--shop-border-color)'} show={!isMobile && isActive ? 'block' : 'none'}/>
                                 <IconWithText
-                                    text={isMobile || isScroll ? "" : "购物车"}
-                                    icon={<svg xmlns="http://www.w3.org/2000/svg"
-                                               width="24" height="24" viewBox="0 0 24 24" fill="none"
-                                               stroke="currentColor" strokeWidth="1.5" strokeLinecap="round"
-                                               strokeLinejoin="round"
-                                               className="icon icon-tabler icons-tabler-outline icon-tabler-shopping-cart">
+                                    text={isMobile || isScroll ? "" : "收藏商品"}
+                                    icon={<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                               viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                                               strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
+                                               className="icon icon-tabler icons-tabler-outline icon-tabler-hearts">
                                         <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                                        <path d="M6 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0"/>
-                                        <path d="M17 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0"/>
-                                        <path d="M17 17h-11v-14h-2"/>
-                                        <path d="M6 5l14 1l-1 7h-13"/>
+                                        <path
+                                            d="M14.017 18l-2.017 2l-7.5 -7.428a5 5 0 1 1 7.5 -6.566a5 5 0 0 1 8.153 5.784"/>
+                                        <path
+                                            d="M15.99 20l4.197 -4.223a2.81 2.81 0 0 0 0 -3.948a2.747 2.747 0 0 0 -3.91 -.007l-.28 .282l-.279 -.283a2.747 2.747 0 0 0 -3.91 -.007a2.81 2.81 0 0 0 -.007 3.948l4.182 4.238z"/>
                                     </svg>}
                                 />
                             </>
@@ -117,7 +116,8 @@ const Navs: React.FC = () => {
                     {({isActive}) => {
                         return (
                             <>
-                                <NavItemBg color={'var(--shop-border-color)'} show={!isMobile && isActive ? 'block' : 'none'}/>
+                                <NavItemBg color={'var(--shop-border-color)'}
+                                           show={!isMobile && isActive ? 'block' : 'none'}/>
                                 <IconWithText
                                     text={isMobile || isScroll ? "" : "我的订单"}
                                     icon={<svg xmlns="http://www.w3.org/2000/svg"
--- src/components/TrolleyItem.tsx
+++ src/components/TrolleyItem.tsx
@@ -1,14 +1,48 @@
 import "./css/TrolleyItem.css"
+import React from "react";
 
-const TrolleyItem = () => {
+interface TrolleyItemProps {
+    imageUrl?: string,
+    title?: string,
+    info?: string,
+    price?: number,
+}
+
+const TrolleyItem: React.FC<TrolleyItemProps>  = ({imageUrl = "",title="未命名",info = "缺少简介",price=0}) => {
     return (
         <>
             <div className="Trolley-Item">
                 <div className="form-check">
-                    <input className="form-check-input m-1" type="checkbox" value="" id="defaultCheck1"/>
-                    <label className="form-check-label" htmlFor="defaultCheck1">
-                        checkbox
-                    </label>
+                    <div className="flex-wrap" style={{width:'40px'}}>
+                        <input className="form-check-input m-1" type="checkbox" value="" id="defaultCheck1"/>
+                    </div>
+                    <div className="form-check-item">
+                        {imageUrl ?
+                            <img style={{maxHeight: "80px", maxWidth: "80px"}} className={'img3'} src={imageUrl}
+                                 alt="预览图"/>
+                            :
+                            <svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" viewBox="0 0 24 24"
+                                 fill="none"
+                                 stroke="currentColor"
+                                 strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
+                                 className="icon icon-tabler icons-tabler-outline icon-tabler-brand-airtable">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                                <path d="M3 10v8l7 -3v-2.6z"/>
+                                <path d="M3 6l9 3l9 -3l-9 -3z"/>
+                                <path d="M14 12.3v8.7l7 -3v-8z"/>
+                            </svg>
+                        }
+                        <div className={'d-block item-info'} style={{textAlign: 'left', marginLeft: '1rem'}}>
+                            <h3>{title}</h3>
+                            <p>{info}</p>
+                        </div>
+                    </div>
+                    <div style={{alignItems: 'center', justifyContent: 'center'}}>
+                        <div style={{flex: 2}}></div>
+                        <label style={{color: 'red', marginRight: '1rem'}}>￥{price.toFixed(2)}</label>
+                        <a className={'btn btn-gold truncate text-truncate'}>结算</a>&nbsp;
+                        <a className={'btn btn-gold truncate text-truncate'}>删除</a>
+                    </div>
                 </div>
             </div>
         </>
