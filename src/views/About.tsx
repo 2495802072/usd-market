4685965f3c6a1d43ef3496bf117cc8d5f36d22ea
顾霖轩 <2495802072@qq.com>
2025-02-17 17:03:38 +0800

    About信息界面完善

--- src/views/About.tsx
+++ src/views/About.tsx
@@ -14,6 +14,8 @@ function About(){
                 </a>
             </div>
             <h1 style={{color: 'var(--shop-font-color)'}}>Vite + React</h1>
+            <hr/>
+            <h5><label style={{fontWeight:"bold"}}>Author: </label><a href="https://github.com/2495802072"> 秦彦悦(笔名：顾霖轩)</a> </h5>
         </>
     )
 }
